578dfbefd52be6898e57685b4bcb7bce5fff84a6
Braden M. Kelley <redbmk@gmail.com>
2018-05-27 08:23:32 -0700

    add automated application registration

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const OktaJwtVerifier = require('@okta/jwt-verifier')
+const okta = require('@okta/okta-sdk-nodejs')
+const oktaClient = new okta.Client({
+  orgUrl: process.env.ORG_URL,
+  token: process.env.TOKEN,
+})
 
 const app = express()
 
@@ -18,6 +23,35 @@ app.get('/', async (req, res) => {
 
     await oktaJwtVerifier.verifyAccessToken(token)
     res.json('Hello World!')
+  } catch (error) {
+    res.json({ error: error.message })
+  }
+})
+
+app.get('/register/:label', async (req, res) => {
+  try {
+    const application = await oktaClient.createApplication({
+      name: 'oidc_client',
+      label: req.params.label,
+      signOnMode: 'OPENID_CONNECT',
+      credentials: {
+        oauthClient: {},
+      },
+      settings: {
+        oauthClient: {
+          grant_types: ['client_credentials'],
+          application_type: 'service',
+        },
+      },
+    })
+
+    const { client_id, client_secret } = application.credentials.oauthClient
+
+    res.json({
+      client_id,
+      client_secret,
+      request_token_url: `${process.env.ISSUER}/v1/token`,
+    })
   } catch (error) {
     res.json({ error: error.message })
   }
